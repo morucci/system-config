bb3fae0cb51786f9866a4d469e26de1703f466c8
Fabien Boucher <fabien.boucher@enovance.com>
2015-03-23 17:33:36 +0000

    Provide an alternate project-config repo

--- manifests/site.pp
+++ manifests/site.pp
@@ -19,7 +19,7 @@ node default {
 # Node-OS: precise
 node 'node1.test.localdomain' {
   class { 'openstack_project::jenkins':
-    project_config_repo     => 'https://git.openstack.org/openstack-infra/project-config',
+    project_config_repo     => 'https://github.com/morucci/project-config.git',
     jenkins_jobs_password   => hiera('jenkins_jobs_password', 'XXX'),
     jenkins_ssh_private_key => hiera('jenkins_ssh_private_key_contents', 'XXX'),
     ssl_cert_file_contents  => hiera('jenkins_ssl_cert_file_contents', 'XXX'),
