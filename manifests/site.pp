1c3f974c7ef42464d18c7fa5b8b1e6ef1d19dba7
Fabien Boucher <fabien.boucher@enovance.com>
2015-03-27 14:25:11 +0000

    Add zuul and zuul-merger in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,28 +43,56 @@ node 'jenkins.test.localdomain' {
   }
 }
 
+node 'zuul.test.localdomain' {
+  class { 'openstack_project::zuul_prod':
+    project_config_repo           => 'https://github.com/morucci/project-config.git',
+    gerrit_server                 => 'gerrit.test.localdomain',
+    gerrit_user                   => 'jenkins',
+    gerrit_ssh_host_key           => hiera('fake_rsa_ssh_public_key_contents', 'XXX'),
+    zuul_ssh_private_key          => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
+    status_url                    => 'http://status.test.localdomain/zuul/',
+    proxy_ssl_cert_file_contents  => hiera('fake_ssl_cert_file_contents', 'XXX'),
+    proxy_ssl_key_file_contents   => hiera('fake_ssl_key_file_contents', 'XXX'),
+    proxy_ssl_chain_file_contents => hiera('fake_ssl_chain_file_contents', 'XXX'),
+    zuul_url                      => 'http://zuul.test.localdomain/p',
+    gearman_workers               => ['jenkins.test.localdomain'],
+    gerrit_ident                  => "gerrit.test.localdomain",
+  }
+}
+
+node 'zm.test.localdomain' {
+  class { 'openstack_project::zuul_merger':
+    gearman_server       => 'zuul.test.localdomain',
+    gerrit_server        => 'gerrit.test.localdomain',
+    gerrit_user          => 'jenkins',
+    gerrit_ssh_host_key  => hiera('fake_rsa_ssh_public_key_contents', 'XXX'),
+    zuul_ssh_private_key => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
+    sysadmins            => hiera('sysadmins', []),
+  }
+}
+
 node 'gerrit.test.localdomain' {
   class { 'openstack_project::gerrit':
-      mysql_host => "mysql.test.localdomain",
-      mysql_password => hiera('mysql_gerrit_password', 'XXX'),
-      serveradmin => 'toto@gerrit.test.localdomain',
-      ssl_cert_file_contents => hiera('fake_ssl_cert_file_contents', 'XXX'),
-      ssl_key_file_contents   => hiera('fake_ssl_key_file_contents', 'XXX'),
-      ssl_chain_file_contents => hiera('fake_ssl_chain_file_contents', 'XXX'),
-      ssh_dsa_key_contents => hiera('fake_dsa_ssh_private_key_contents', 'XXX'),
-      ssh_dsa_pubkey_contents => hiera('fake_dsa_ssh_public_key_contents', 'XXX'),
-      ssh_rsa_key_contents => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
-      ssh_rsa_pubkey_contents => hiera('fake_rsa_ssh_public_key_contents', 'XXX'),
-      ssh_project_rsa_key_contents => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
-      ssh_project_rsa_pubkey_contents => hiera('fake_rsa_ssh_public_key_contents', 'XXX'),
-      ssh_welcome_rsa_key_contents => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
-      ssh_welcome_rsa_pubkey_contents => hiera('fake_rsa_ssh_public_key_contents', 'XXX'),
-      ssh_replication_rsa_key_contents => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
+      mysql_host                          => "mysql.test.localdomain",
+      mysql_password                      => hiera('mysql_gerrit_password', 'XXX'),
+      serveradmin                         => 'toto@gerrit.test.localdomain',
+      ssl_cert_file_contents              => hiera('fake_ssl_cert_file_contents', 'XXX'),
+      ssl_key_file_contents               => hiera('fake_ssl_key_file_contents', 'XXX'),
+      ssl_chain_file_contents             => hiera('fake_ssl_chain_file_contents', 'XXX'),
+      ssh_dsa_key_contents                => hiera('fake_dsa_ssh_private_key_contents', 'XXX'),
+      ssh_dsa_pubkey_contents             => hiera('fake_dsa_ssh_public_key_contents', 'XXX'),
+      ssh_rsa_key_contents                => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
+      ssh_rsa_pubkey_contents             => hiera('fake_rsa_ssh_public_key_contents', 'XXX'),
+      ssh_project_rsa_key_contents        => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
+      ssh_project_rsa_pubkey_contents     => hiera('fake_rsa_ssh_public_key_contents', 'XXX'),
+      ssh_welcome_rsa_key_contents        => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
+      ssh_welcome_rsa_pubkey_contents     => hiera('fake_rsa_ssh_public_key_contents', 'XXX'),
+      ssh_replication_rsa_key_contents    => hiera('fake_rsa_ssh_private_key_contents', 'XXX'),
       ssh_replication_rsa_pubkey_contents => hiera('fake_rsa_ssh_public_key_contents', 'XXX'),
-      email => 'toto@gerrit.test.localdomain',
-      war => 'http://tarballs.openstack.org/ci/test/gerrit-v2.8.4.19.7c824ff.war',
-      replicate_local => false,
-      testmode => true,
+      email                               => 'toto@gerrit.test.localdomain',
+      war                                 => 'http://tarballs.openstack.org/ci/test/gerrit-v2.8.4.19.7c824ff.war',
+      replicate_local                     => false,
+      testmode                            => true,
   }
 }
 # vim:sw=2:ts=2:expandtab:textwidth=79
